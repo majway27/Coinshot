38ff4bb1e1077346c4413bb2c7aaf4ae7435f64a
majway27 <robert.p.may@gmail.com>
2019-09-10 05:36:08 -0600

    Start Persistent Data Container support. Get Plugin mechanism

--- src/main/java/com/majway/coinshot/Coinshot.java
+++ src/main/java/com/majway/coinshot/Coinshot.java
@@ -1,15 +1,26 @@
 package com.majway.coinshot;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Coinshot extends JavaPlugin {
+
+    private static Coinshot pluginCoinshot;
+
     @Override
     public void onEnable() {
         getLogger().info("onEnable is called!");
+
+        pluginCoinshot = this;
         this.getCommand("CoinshotNewBuilding").setExecutor(new NewBuildingCommand());
     }
     @Override
     public void onDisable() {
+        pluginCoinshot = null;
         getLogger().info("onDisable is called!");
+    }
+
+    public static Coinshot getInstance() {
+        return pluginCoinshot;
     }
 }
--- src/main/java/com/majway/coinshot/NewBuildingCommand.java
+++ src/main/java/com/majway/coinshot/NewBuildingCommand.java
@@ -1,20 +1,23 @@
 package com.majway.coinshot;
 
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
 
 import java.util.ArrayList;
 
 public class NewBuildingCommand implements CommandExecutor {
-    // This method is called, when somebody uses our command
+
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
+            NamespacedKey key = new NamespacedKey(Coinshot.getInstance(), "our-custom-key");
 
             ItemStack cornerStone = new ItemStack(Material.BEDROCK, 1);
             ItemMeta itemStackMeta = cornerStone.getItemMeta();
